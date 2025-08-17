3bbdbc3bdeba420aaf779df2c2f766e10e2405cb
pranvking <pranavkarmacharya@gmail.com>
2025-08-17 23:04:42 +0545

    contact validation

--- pageObjects/contact.po.js
+++ pageObjects/contact.po.js
@@ -1,19 +1,67 @@
-const{expect} = require('@playwright/test');
+const { expect } = require("@playwright/test"); 
+
 exports.ContactPage = class ContactPage {
-    constructor(page){
-       this.contactButton='//button[@id="add-contact"]';
-       this.firstName='//input[@id="firstName"]';
-       this.lastName='//input[@id="lastName"]';
-       this.dateofBirth='//input[@id="birthdate"]';
-       this.email='//input[@id="email"]';
-       this.phone='//input[@id="phone"]';
-       this.street1='//input[@id="street1"]';
-       this.street2='//input[@id="street2"]';
-       this.city='//input[@id="city"]';
-       this.stateProvince='//input[@id="stateProvince"]';
-       this.postalCode='//input[@id="postalCode"]';
-       this.country='//input[@id="country"]';
-       this.submitButton='//button[@id="submit"]';
-       this.cancleButton='//button[@id="cancel"]';
-    }
-}
+  constructor(page) {
+    this.page = page; 
+
+    this.usernameInput = "#email";
+    this.passwordInput = '//input[@placeholder="Password"]';
+    this.loginButton = '//button[@id="submit"]';
+    this.contactButton = '//button[@id="add-contact"]';
+    this.firstName = '//input[@id="firstName"]';
+    this.lastName = '//input[@id="lastName"]';
+    this.dateofBirth = '//input[@id="birthdate"]';
+    this.email = '//input[@id="email"]';
+    this.phone = '//input[@id="phone"]';
+    this.street1 = '//input[@id="street1"]';
+    this.street2 = '//input[@id="street2"]';
+    this.city = '//input[@id="city"]';
+    this.stateProvince = '//input[@id="stateProvince"]';
+    this.postalCode = '//input[@id="postalCode"]';
+    this.country = '//input[@id="country"]';
+    this.submitButton = '//button[@id="submit"]';
+    this.cancelButton = '//button[@id="cancel"]';
+    this.confirmContact = '//p[contains(text(),"Click on any contact to view the Contact Details")]';
+  }
+
+  async login(username, password) {
+    await this.page.locator(this.usernameInput).fill(username);
+    await this.page.locator(this.passwordInput).fill(password);
+    await this.page.locator(this.loginButton).click();
+  }
+
+  async addContact(
+    FirstName,
+    LastName,
+    DateofBirth,
+    Email,
+    Phone,
+    StreetAddress1,
+    StreetAddress2,
+    City,
+    State,
+    PostalCode,
+    Country
+  ) {
+    await this.page.locator(this.contactButton).click();
+    await this.page.locator(this.firstName).fill(FirstName);
+    await this.page.locator(this.lastName).fill(LastName);
+    await this.page.locator(this.dateofBirth).fill(DateofBirth);
+    await this.page.locator(this.email).fill(Email);
+    await this.page.locator(this.phone).fill(Phone);
+    await this.page.locator(this.street1).fill(StreetAddress1);
+    await this.page.locator(this.street2).fill(StreetAddress2);
+    await this.page.locator(this.city).fill(City);
+    await this.page.locator(this.stateProvince).fill(State);
+    await this.page.locator(this.postalCode).fill(PostalCode);
+    await this.page.locator(this.country).fill(Country);
+    await this.page.locator(this.submitButton).click();
+  }
+
+  async verifyValidContact() {
+    const confirmContact = this.page.locator(this.confirmContact);
+    await expect(confirmContact).toHaveText(
+      "Click on any contact to view the Contact Details"
+    );
+  }
+};
--- tests/contact.spec.js
+++ tests/contact.spec.js
@@ -1,32 +1,33 @@
 import { expect, test } from "@playwright/test";
 import { ContactPage } from "../pageObjects/contact.po.js"; 
-const testData =require('../fixtures/contactFixtures.json');
+const testData = require('../fixtures/contactFixtures.json');
 
-test.beforeEach(async({page})=>{
-    await page.goto('/');
-})
+test.beforeEach(async ({ page }) => {
+  await page.goto('/');
+});
 
 test.describe('Valid contact tests', () => {
-    test('Valid Contact',async ({page}) => {
-        const contact=new ContactPage(page);
-        await login.contact(
-            testData.validContact.FirstName,
-            testData.validContact.LastName,
-            testData.validContact.DateofBirth,
-            testData.validContact.Email,
-            testData.validContact.Phone,
-            testData.validContact.StreetAddress1,
-            testData.validContact.StreetAddress2,
-            testData.validContact.City,
-            testData.validContact.State,
-            testData.validContact.StreetAddress2,
-            testData.validContact.City,
-            test.validContact.StateofProvience,
-            testData.validContact.PostalCode,
-            testData.validContact.postalCode,
-            testData.validContact.Country,
-            testData.validContact.Country
-        );
-        await login.verifyValidContact();
-    });
-})
+  test('Valid Contact', async ({ page }) => {
+    const contact = new ContactPage(page);
+
+
+    await contact.login("pranavkarmacharya@gmail.com", "pranavkarmacharya123");
+
+
+    await contact.addContact(
+      testData.validContact.FirstName,
+      testData.validContact.LastName,
+      testData.validContact.DateofBirth,
+      testData.validContact.Email,
+      testData.validContact.Phone,
+      testData.validContact.StreetAddress1,
+      testData.validContact.StreetAddress2,
+      testData.validContact.City,
+      testData.validContact.StateofProvience, 
+      testData.validContact.postalCode,     
+      testData.validContact.Country
+    );
+
+    await contact.verifyValidContact();
+  });
+});
